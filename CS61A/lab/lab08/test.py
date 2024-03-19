9f4f4fa45090a904b162c6c532eade2d4fd52118
smart-coco <1044357360@qq.com>
2024-03-19 23:34:52 +0800

    补交cs50,cs61a_20fa,nand

--- CS61A/lab/lab08/test.py
+++
@@ -1,6 +0,0 @@
-count=0
-while True:
-    count+=1
-    print(count)
-    if count>10:
-        break
--- CS61A/project/ants/test.py
+++
@@ -1,9 +0,0 @@
-class X:
-    def __init__(self,armor=2):
-        self.a=armor
-def p(x):
-    print(self.a*x)
-
-aa=X()
-aa.f=p
-aa.f()
